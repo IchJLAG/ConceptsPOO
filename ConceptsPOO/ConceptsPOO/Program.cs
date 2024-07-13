ae554239f449cf3780a1083fb8077ad8a9401b15
Jose Alvarez Gaona <jose_luis_alvarez_g@hotmail.com>
2024-07-12 19:00:48 -0500

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using ConceptsPOO;
+
+Console.WriteLine("Primeros pasos");
+Console.WriteLine("============");
+
+try
+{
+	Console.WriteLine(new Date(2022, 02, 22));
+	Console.WriteLine(new Date(2024, 07, 04));
+	Console.WriteLine(new Date(2024, 11, 22));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
